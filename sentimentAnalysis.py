d410144c1462f72d72d03d84d920144a3f810575
JBrandt12 <59374842+JBrandt12@users.noreply.github.com>
2023-12-01 17:42:58 -0600

    Update sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,14 +1,10 @@
 import pandas as pd 
-import numpy as np 
 import nltk 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-plt.style.use('ggplot') 
 from nltk.sentiment import SentimentIntensityAnalyzer 
+import os
+nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
 
 sia = SentimentIntensityAnalyzer() 
-# newshealines = ["Bitcoin Tops $37K for First Time Since May 2022 as Short Squeeze Bumps Prices Amid BTC ETF Optimism","UBS Group’s Wealthy Clients Can Now Trade Some Crypto ETFs in Hong Kong: Bloomberg","2 Years Ago, Bitcoin Hit an All-Time High. Is Another Rally on the Way?", "FTX's FTT Token Jumps 90% on Gensler Comments" , "U.S. SEC Said to Open Talks with Grayscale on Spot Bitcoin ETF Push"]
-
 def analyzier(newshealines): 
     res = []
     for i in newshealines: 
@@ -25,8 +21,5 @@ def analyzier(newshealines):
     if pd.isna(y): 
         y=0 
     return (x,y)
-
-
-# analyzier(newshealines) 
 
 
